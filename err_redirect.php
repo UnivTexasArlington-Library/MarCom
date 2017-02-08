06f18bd06b5b4754778efe7605b6988c7e2cd5fa
aleverenz <leverenz@uta.edu>
2017-02-08 14:10:33 -0600

    htaccess update

--- err_redirect.php
+++ err_redirect.php
@@ -1,3 +1,20 @@
 <?php
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+
+# if the request was not SSL
+if (empty($_SERVER[ "HTTPS" ]))
+{
+  # and if there is a script URI to redirect to
+  if( !empty( $_SERVER[ "SCRIPT_URI" ] ))
+  {
+    # forward them to the SSL version of the script they requested
+    $new_url = str_ireplace( "http://", "https://", $_SERVER[ "SCRIPT_URI" ] );
+    if (!empty($_SERVER["REDIRECT_QUERY_STRING"])) $new_url .= "?" . $_SERVER['REDIRECT_QUERY_STRING'];
+    header( "Location: ${new_url}" );
+    exit;
+  }
+}
+
+# if we didn't redirect them, show the normal 403 error page
+include "/opt/libwebfs/web-libweb-nm-test/library-test.uta.edu/node/533";
+
 ?>
